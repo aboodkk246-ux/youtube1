c14adcc5aafa799cd52499872739dc27878435ee
aboodkk246-ux <aboodkk246@gmail.com>
2026-03-26 21:08:33 +0300

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,22 @@
+
+package javaapplication3;
+
+import java.util.Scanner;
+
+public class JavaApplication3 {
+ public static void main(String[] args ){
+Scanner input=new Scanner(System.in);
+double m1 , m2, m3 ,avg,sum;
+m1=input.nextDouble();
+m2=input.nextDouble();     
+m3=input.nextDouble();   
+sum=m1+m2+m3;
+avg=sum/3;
+System.out.println("sum=" +sum);
+System.out.println("average=" +avg);
+ }
+ 
+}
+  
+       
+    
